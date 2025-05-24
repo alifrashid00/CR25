f152136a42578b8857744c94d48fb08e47d6db4f
jubaer36 <abdullahaljubaergem@gmail.com>
2025-05-24 15:55:02 +0600

    List of individual items

--- student-marketplace/src/App.jsx
+++ student-marketplace/src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./components/SignUp.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./components/Login.jsx";
 import Listing from "./pages/listing/listing.jsx";
+import ListingDetail from "./pages/listing/[id].jsx";
 import Sell from "./pages/sell/sell.jsx";
 
 function App() {
@@ -13,8 +14,8 @@ function App() {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/listing" element={<Listing />} />
+                <Route path="/listing/:id" element={<ListingDetail />} />
                 <Route path="/sell" element={<Sell />} />
-
             </Routes>
         </Router>
     );
